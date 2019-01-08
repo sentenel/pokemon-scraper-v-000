ccdb48fafb409408f2d07b4ff4f186c26fb45c4c
Seth Wax <sethlwax@gmail.com>
2019-01-08 10:45:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,7 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    pokemon = Pokemon.new(id: @@all.length + 1, name: name, type: type, db: db)
+    pokemon = Pokemon.new(id: @@all.length, name: name, type: type, db: db)
     db.execute("INSERT INTO pokemon VALUES (#{pokemon.id}, '#{name}', '#{type}');")
   end
 
