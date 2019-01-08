a9c2490fc88dd894cc80e7776f53a1d743fe35bc
Seth Wax <sethlwax@gmail.com>
2019-01-08 10:52:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id)
+  def self.find(id, db)
     db.execute("SELECT * FROM pokemon WHERE id = ?", id)
   end
 
